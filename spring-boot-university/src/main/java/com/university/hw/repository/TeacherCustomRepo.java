35675074f373076ef8eacf7ffaac20604ce1cb12
Fan Luo <fluo4@stevens.edu>
2022-12-04 16:45:09 -0500

    Delete spring-boot-university directory

--- spring-boot-university/src/main/java/com/university/hw/repository/TeacherCustomRepo.java
+++
@@ -1,9 +0,0 @@
-package com.university.hw.repository;
-
-import com.university.hw.domain.entity.Teacher;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeacherCustomRepo {
-    Teacher insert(Teacher teacher);
-}
