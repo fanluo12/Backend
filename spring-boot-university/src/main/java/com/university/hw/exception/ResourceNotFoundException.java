2e8c6fce3027ea05788a22bbd25480ea34588037
Fan Luo <fluo4@stevens.edu>
2022-12-05 15:41:32 -0500

    Delete spring-boot-university directory

--- spring-boot-university/src/main/java/com/university/hw/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.university.hw.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- spring-boot-university/src/main/java/com/university/hw/repository/StudentCustomRepo.java
+++
@@ -1,9 +0,0 @@
-package com.university.hw.repository;
-
-import com.university.hw.domain.entity.Student;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentCustomRepo {
-    Student insert(Student student);
-}
