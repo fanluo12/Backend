675fab9a1c4b76fd578ec181d1d26e3b578b0ae9
Fan Luo <fluo4@stevens.edu>
2022-12-12 20:02:19 -0500

    Delete weather/university directory

--- weather/university/src/main/java/com/example/university/service/TeacherService.java
+++
@@ -1,9 +0,0 @@
-package com.example.university.service;
-
-import com.example.university.domain.TeacherDTO;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface TeacherService {
-    TeacherDTO[] getAllTeachers();
-}
