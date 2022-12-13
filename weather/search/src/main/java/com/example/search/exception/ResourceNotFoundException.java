b0d07c5a5959095f2e60664754c4ca954b6e541d
Fan Luo <fluo4@stevens.edu>
2022-12-12 20:02:46 -0500

    Delete weather/search directory

--- weather/search/src/main/java/com/example/search/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.search.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
